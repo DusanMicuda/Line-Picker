e2f6a3a7a46514a29ac848e86a1088f7746cb03c
DusanMicuda <dusanmicuda11@gmail.com>
2021-12-03 18:09:38 +0100

    implement importFragment

--- app/src/main/java/com/micudasoftware/linepicker/ImportFragment.kt
+++ app/src/main/java/com/micudasoftware/linepicker/ImportFragment.kt
@@ -1,59 +1,82 @@
 package com.micudasoftware.linepicker
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import com.micudasoftware.linepicker.databinding.FragmentImportBinding
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import org.apache.poi.ss.usermodel.Row
+import org.apache.poi.ss.usermodel.Workbook
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ImportFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ImportFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+    private val xls = "application/vnd.ms-excel"
+    private val xlsx = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_import, container, false)
+    ): View {
+        val binding: FragmentImportBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_import, container, false)
+        binding.handler = this
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ImportFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ImportFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            result.data?.data?.let { data ->
+                readExcelData(data)
+                view?.let { Navigation.findNavController(it).navigate(R.id.action_importFragment_to_randomizeFragment) }
             }
+        }
+    }
+
+    fun importFile() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "*/*"
+        val mimetypes = arrayOf(xlsx, xls)
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimetypes)
+
+        resultLauncher.launch(intent)
+    }
+
+    /*
+    3 stlpce, preklady rozdelit enterom, v subore 10 stlpcov, prve dva + 8 preklaadov
+    prvy riadok zadanie, druhy hlavicka
+     */
+
+    private fun readExcelData(excelFile: Uri) {
+        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        val inputStream = context?.contentResolver?.openInputStream(excelFile)
+
+        val workbook: Workbook =
+            when (context?.contentResolver?.getType(excelFile)) {
+                xls -> HSSFWorkbook(inputStream)
+                xlsx -> XSSFWorkbook(inputStream)
+                else -> return
+            }
+
+        val sheet = workbook.getSheetAt(0)
+        for (row: Row in sheet) {
+            when (row.rowNum) {
+                0 -> if (row.cellIterator().hasNext())
+                    viewModel.assignment.value = row.cellIterator().next().stringCellValue
+                1 -> viewModel.header.value = row
+                else -> viewModel.rows.value?.add(row)
+            }
+        }
     }
 }
--- app/src/main/java/com/micudasoftware/linepicker/MainViewModel.kt
+++ app/src/main/java/com/micudasoftware/linepicker/MainViewModel.kt
@@ -1,6 +1,16 @@
 package com.micudasoftware.linepicker
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import org.apache.poi.ss.usermodel.Row
 
 class MainViewModel : ViewModel() {
+
+    val assignment = MutableLiveData<String>()
+    val header = MutableLiveData<Row>()
+    val rows = MutableLiveData<ArrayList<Row>>()
+
+    init {
+        rows.value = ArrayList()
+    }
 }
